29dbda584587eefec2deb4507f0284daa032ec96
Nikhil Heda <nikhilheda96@gmail.com>
2016-06-05 14:41:50 +0530

    snake length rules

--- code.py
+++ code.py
@@ -23,8 +23,9 @@ block_size = 10
 
 font = pygame.font.SysFont(None, 25)
 
-def snake(lead_x, lead_y, block_size):
-	pygame.draw.rect(gameDisplay, green, [lead_x, lead_y, block_size, block_size])
+def snake(block_size, snakelist):
+	for XnY in snakelist:
+		pygame.draw.rect(gameDisplay, green, [XnY[0], XnY[1], block_size, block_size])
 
 def message_to_screen(msg, color):
 	screen_text = font.render(msg, True, color)
@@ -81,7 +82,14 @@ def gameLoop():
 		
 		gameDisplay.fill(white)
 		pygame.draw.rect(gameDisplay, red, [randAppleX, randAppleY, block_size, block_size])
-		snake(lead_x, lead_y, block_size)
+		
+		snakeList = []
+		snakeHead = []
+		snakeHead.append(lead_x)
+		snakeHead.append(lead_y)
+		snakeList.append(snakeHead)
+		snake(block_size, snakeList)
+		
 		pygame.display.update()
 		
 		if lead_x == randAppleX and lead_y == randAppleY:
