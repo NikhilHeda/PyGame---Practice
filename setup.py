609e113c1b6c173087d5908b9704d775bad35fea
Nikhil Heda <nikhilheda96@gmail.com>
2016-06-05 18:24:18 +0530

    Added the packaging code (setup.py)

---
+++ setup.py
@@ -0,0 +1,11 @@
+import cx_Freeze
+
+executables = [cx_Freeze.Executable("code.py")]
+
+cx_Freeze.setup(
+	name = "Slither",
+	options = {"build_exe" : {"packages":["pygame"], "include_files":["apple.png", "snake.png"]}},
+	description = "Slither Game",
+	executables = executables
+)
+
