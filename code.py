020042f8d48bb4339da26ac0fb37119b0001e3d2
Nikhil Heda <nikhilheda96@gmail.com>
2016-06-05 16:54:56 +0530

    Added font style and size functionality

--- code.py
+++ code.py
@@ -25,7 +25,9 @@ block_size = 20
 
 direction = "right"
 
-font = pygame.font.SysFont(None, 25)
+smallfont = pygame.font.SysFont("comicsansms", 25)
+medfont = pygame.font.SysFont("comicsansms", 50)
+largefont = pygame.font.SysFont("comicsansms", 80)
 
 def snake(block_size, snakelist):
 	
@@ -43,14 +45,17 @@ def snake(block_size, snakelist):
 	for XnY in snakelist[:-1]:
 		pygame.draw.rect(gameDisplay, green, [XnY[0], XnY[1], block_size, block_size])
 
-def text_objects(text, color):
-	textSurface = font.render(text, True, color)
+def text_objects(text, color, size):
+	if size == "small":
+		textSurface = smallfont.render(text, True, color)
+	elif size == "medium":
+		textSurface = medfont.render(text, True, color)
+	elif size == "large":
+		textSurface = largefont.render(text, True, color)
 	return textSurface, textSurface.get_rect()
-		
-def message_to_screen(msg, color, y_displace = 0):
-	# screen_text = font.render(msg, True, color)
-	# gameDisplay.blit(screen_text, [display_width / 2, display_height / 2])
-	textSurf, textRect = text_objects(msg, color)
+
+def message_to_screen(msg, color, y_displace = 0, size = "small"):
+	textSurf, textRect = text_objects(msg, color, size)
 	textRect.center = (display_width / 2), (display_height / 2) + y_displace
 	gameDisplay.blit(textSurf, textRect)
 
@@ -75,8 +80,8 @@ def gameLoop():
 	
 		while gameOver:
 			gameDisplay.fill(white)
-			message_to_screen("Game Over.", red, y_displace = -50)
-			message_to_screen("Press C to Continue, Q to quit.", black)
+			message_to_screen("Game Over.", red, y_displace = -50, size = "large")
+			message_to_screen("Press C to Continue, Q to quit.", black, y_displace = 50, size = "medium")
 			pygame.display.update()
 			for event in pygame.event.get():
 				if event.type == pygame.QUIT:
