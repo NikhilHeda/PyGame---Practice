86a487b693459dd03610441ec70eb54446ce55b1
Nikhil Heda <nikhilheda96@gmail.com>
2016-06-07 00:51:06 +0530

    Handled Collision and explosion functionality

--- code.py
+++ code.py
@@ -190,6 +190,32 @@ def tank(x, y, turPos):
   
 def barrier(xlocation, randomHeight, barrier_width):
 	pygame.draw.rect(gameDisplay, black, [xlocation, display_height - randomHeight, barrier_width, randomHeight])
+	
+def explosion(x, y):
+	explode = True
+	
+	while explode:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+				quit()
+				
+		startPoint = x, y
+		
+		colorChoices = [red, light_red, yellow, light_yellow]
+		
+		magnitude = 1
+		
+		while magnitude < 50:
+			exploding_bit_x = x + random.randrange(-1*magnitude, magnitude)
+			exploding_bit_y = y + random.randrange(-1*magnitude, magnitude)
+			pygame.draw.circle(gameDisplay, colorChoices[random.randrange(0, 4)], (exploding_bit_x, exploding_bit_y), random.randrange(1, 5))
+			magnitude += 1
+			
+			pygame.display.update()
+			clock.tick(100)
+			
+		explode = False
 
 def	fireShell(xy, tankx, tanky, turPos, gun_power):
 	fire = True
@@ -208,6 +234,9 @@ def	fireShell(xy, tankx, tanky, turPos, gun_power):
 		startingShell[1] += int((((startingShell[0] - xy[0]) * 0.015 / (gun_power / 50)) ** 2) - (turPos + turPos / (12 - turPos)) )
 		
 		if startingShell[1] > display_height:
+			hit_x = int((startingShell[0] * display_height)/startingShell[1])
+			hit_y = int(display_height)
+			explosion(hit_x, hit_y)
 			fire = False
 		
 		pygame.display.update()
